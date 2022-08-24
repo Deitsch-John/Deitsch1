3c3a5ee6bd78af847ba2dc7eceb7fd9762ad038f
Deitsch-John <jfd77@cornell.edu>
2022-08-23 21:56:12 -0400

    initial commits

--- R/obs_to_coords.R
+++ R/obs_to_coords.R
@@ -1,5 +1,15 @@
 
 
+#' convert a dataframe with lat and long columns to an sf object
+#'
+#' @param df dataframe with coordinates column
+#' @param coord_col column with coordinates
+#' @param crs_add desired coordinate reference system
+#'
+#' @return simple features dataframe
+#' @export
+#'
+#' @examples example.sf <- obs_to_coords(example.df, coords, 4269)
 obs_to_coords <- function(df, coord_col, crs_add){
   coords.sfg <- df$coords
   coords.sfc <- st_sfc(coords.sfg, crs = crs_add)
--- R/view_sf.R
+++ R/view_sf.R
@@ -1,5 +1,14 @@
 
+#' view a spatial dataframe in the R studio viewer
+#'
+#' @param df an sf dataframe with a geometry column
+#'
+#' @return nothing
+#' @export
+#'
+#' @examples view_sf(USA_counties)
 view_sf <- function(df){
+
   df%>%
     st_drop_geometry()%>%
     View()
